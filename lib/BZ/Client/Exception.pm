2d3929289c4a95f6b747bd70b543dac00480cdff
Dean Hamstead <dean@fragfest.com.au>
2015-09-04 10:11:55 +1000

    Update pod to help with RT91405

--- lib/BZ/Client/Exception.pm
+++ lib/BZ/Client/Exception.pm
@@ -47,11 +47,40 @@ Instead, it throws instances of BZ::Client::Exception.
 
   my $exception = BZ::Client::Exception->new( message     => $message,
                                               http_code   => $httpCode,
-                                              xmlrpc_code => $xmlrpcCode);
+                                              xmlrpc_code => $xmlrpcCode );
 
   BZ::Client::Exception->throw( message     => $message,
                                 http_code   => $httpCode,
-                                xmlrpc_code => $xmlrpcCode);
+                                xmlrpc_code => $xmlrpcCode );
+
+=head1 METHODS
+
+=head2 new
+
+Creates the exception object
+
+=head2 throw
+
+Creates the exception object then dies, so make sure you catch it!
+
+=head2 message
+
+Returns the error message text
+
+=head2 xmlrpc_code
+
+Returns the error code from XMLRPC
+
+=head2 http_code
+
+Returns the http code (200, 404, etc)
+
+=head1 EXAMPLE
+
+ my $client = BZ::Client->new( url => $url, user => $user, password => $pass);
+ $client->login();
+
+ # FIXME need to finish example
 
 =head1 SEE ALSO
 
