a3b3f7457b3b74230b9a621587fee5209c678cf3
Dean Hamstead <dean@fragfest.com.au>
2015-09-17 23:37:10 +1000

    stubs

---
+++ lib/BZ/Client/Bug/Attachment.pm
@@ -0,0 +1,16 @@
+#!/bin/false
+# PODNAME: BZ::Client::Bug::Attachment
+# ABSTRACT: Client side representation of an Attachment to a Bug in Bugzilla
+
+use strict;
+use warnings 'all';
+
+package BZ::Client::Bug::Attachment;
+
+use parent qw( BZ::Client::API );
+
+# See https://www.bugzilla.org/docs/tip/en/html/api/Bugzilla/WebService/Bug.html
+# These are in order as per the above
+
+
+1;
---
+++ lib/BZ/Client/Bug/Comment.pm
@@ -0,0 +1,16 @@
+#!/bin/false
+# PODNAME: BZ::Client::Bug::Comment
+# ABSTRACT: Client side representation of an Comment on a Bug in Bugzilla
+
+use strict;
+use warnings 'all';
+
+package BZ::Client::Bug::Comment;
+
+use parent qw( BZ::Client::API );
+
+# See https://www.bugzilla.org/docs/tip/en/html/api/Bugzilla/WebService/Bug.html
+# These are in order as per the above
+
+
+1;
