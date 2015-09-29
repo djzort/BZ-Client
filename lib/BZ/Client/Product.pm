d79fb69031e4d067afbedb3a726bbd295e02e71c
Dean Hamstead <dean@fragfest.com.au>
2015-09-29 23:10:25 +1000

    Add create and update methods

--- lib/BZ/Client/Product.pm
+++ lib/BZ/Client/Product.pm
@@ -13,6 +13,30 @@ use parent qw( BZ::Client::API );
 
 ## functions
 
+sub create {
+    my($class, $client, $params) = @_;
+    $client->log('debug', __PACKAGE__ . '::create: Creating');
+    my $result = $class->api_call($client, 'Product.create', $params);
+    my $id = $result->{'id'};
+    if (!$id) {
+        $class->error($client, 'Invalid reply by server, expected bug ID.');
+    }
+    $client->log('debug', __PACKAGE__ . "::create: Made $id");
+    return $id
+}
+
+sub update {
+    my($class, $client, $params) = @_;
+    $client->log('debug', __PACKAGE__ . '::update: Updating');
+    my $result = $class->api_call($client, 'Product.update', $params);
+    my $products = $result->{'products'};
+    if (!$products || 'ARRAY' ne ref($products)) {
+        $class->error($client, 'Invalid reply by server, expected array of products details.');
+    }
+    $client->log('debug', __PACKAGE__ . '::update: Updated stuff');
+    return wantarray ? @$products : $products
+}
+
 # convenience function
 sub _get_list {
     my($class, $methodName, $client) = @_;
@@ -141,6 +165,217 @@ of this class are returned by L<BZ::Client::Product::get>.
 
 This section lists the class methods, which are available in this module.
 
+=head2 create
+
+ $id = BZ::Client::Product->create( $client, \%params );
+
+This allows you to create a new product in Bugzilla.
+
+Marked as experimental as of Bugzilla 5.0.
+
+=head3 Parameters
+
+Some params must be set, or an error will be thrown. These params are noted as Required.
+
+=over 4
+
+=item name
+
+I<name> (string) The name of this product. Must be globally unique within Bugzilla.
+
+Required.
+
+=item description
+
+I<description> (string) A description for this product. Allows some simple HTML.
+
+Required.
+
+=item version
+
+I<version> (string) The default version for this product.
+
+Required.
+
+=item has_unconfirmed
+
+I<has_unconfirmed> (boolean) Allow the UNCONFIRMED status to be set on bugs in this product. Default: true.
+
+=item classification
+
+I<classification> (string) The name of the Classification which contains this product.
+
+=item default_milestone
+
+I<default_milestone> (string) The default milestone for this product. Default '---'.
+
+=item is_open
+
+I<is_open> (boolean) True if the product is currently allowing bugs to be entered into it. Default: true.
+
+=item create_series
+
+I<create_series> (boolean) True if you want series for New Charts to be created for this new product. Default: true.
+
+=back
+
+=head3 Returns
+
+The ID of the newly-filed product.
+
+=head3 Errors
+
+=over 4
+
+=item 51 - Classification does not exist
+
+You must specify an existing classification name.
+
+=item 700 - Product blank name
+
+You must specify a non-blank name for this product.
+
+=item 701 - Product name too long
+
+The name specified for this product was longer than the maximum allowed length.
+
+=item 702 - Product name already exists
+
+You specified the name of a product that already exists. (Product names must be globally unique in Bugzilla.)
+
+=item 703 - Product must have description
+
+You must specify a description for this product.
+
+=item 704 - Product must have version
+
+You must specify a version for this product.
+
+=back
+
+=head2 update
+
+ $id = BZ::Client::Product->update( $client, \%params );
+
+This allows you to update a product in Bugzilla.
+
+As of Bugzilla 5.0. this is marked as experimental.
+
+Added in Bugzilla 4.4.
+
+=head3 Parameters
+
+Either L</ids> or L</names> is required to select the bugs you want to update.
+
+All other values change or set something in the product.
+
+=over 4
+
+=item ids
+
+I<ids> (array) Numeric ids of the products you wish to update.
+
+=item names
+
+I<names> (array) Text names of the products that you wish to update.
+
+=item default_milestone
+
+I<default_milestone> (string) When a new bug is filed, what milestone does it
+get by default if the user does not choose one? Must represent a milestone that
+is valid for this product.
+
+=item description
+
+I<description> (string) Update the long description for these products to this value.
+
+=item has_unconfirmed
+
+I<has_unconfirmed> (boolean) Allow the UNCONFIRMED status to be set on bugs in this products.
+
+=item is_open
+
+I<is_open> (boolean) True if the product is currently allowing bugs to be entered into it.
+Otherwise false.
+
+=back
+
+=head3 Returns
+
+An array or arrayref of hashes containing the following:
+
+=over 4
+
+=item id
+
+I<id> (int) The id of the product that was updated.
+
+=item changes
+
+The changes that were actually done on this product. The keys are the names of the
+fields that were changed, and the values are a hash with two keys:
+
+=over 4
+
+=item added
+
+I<added> (string) The value that this field was changed to.
+
+=item removed
+
+I<removed> (string) The value that was previously set in this field.
+
+=back
+
+Note that booleans will be represented with the strings '1' and '0'.
+
+Here's an example of what a return value might look like:
+
+ [
+     {
+         id => 123,
+         changes => {
+             name => {
+                 removed => 'FooName',
+                 added   => 'BarName'
+             },
+             has_unconfirmed => {
+                 removed => '1',
+                 added   => '0',
+             }
+         }
+     },
+     \%etc
+ ],
+
+=back
+
+=head3 Errors
+
+=over 4
+
+=item 700 - Product blank name
+
+You must specify a non-blank name for this product.
+
+=item 701 - Product name too long
+
+The name specified for this product was longer than the maximum allowed length.
+
+=item 702 - Product name already exists
+
+You specified the name of a product that already exists. (Product names must be globally unique in Bugzilla.)
+
+=item 703 - Product must have description
+
+You must specify a description for this product.
+
+=item 705 - Product must define a default milestone
+
+You must define a default milestone.
+
+=back
+
 =head2 get_selectable_products
 
  @products = BZ::Client::Product->get_selectable_products( $client );
@@ -247,6 +482,10 @@ See L</INSTANCE METHODS> for how to use them.
 =head3 Errors
 
 (none)
+
+=head2 get_products
+
+Compatibilty with Bugzilla 3.0 API. Exactly equivalent to L</get>.
 
 =head2 new
 
